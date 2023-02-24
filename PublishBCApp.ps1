22e709b0c39d6242c109a508e3294fdf86ecc23d
mileslishFLA <121807224+mileslishFLA@users.noreply.github.com>
2023-02-24 14:43:24 +0100

    Update PublishBCApp.ps1

--- PublishBCApp.ps1
+++ PublishBCApp.ps1
@@ -2,33 +2,15 @@ $repositoriesPathHerlp = "C:\Users\MileSlishkovikj\source\repos\MyLocalBC\TechIn
 $appName = "GitActionsTechInovBCApp.app"
 $appNameShort = "GitActionsTechInovBCApp"
 $serverInstance = "BC"
-$containerName = "MyLocalBC" 
+$containerName = "MyLocalBC"
 $securePassword = ConvertTo-SecureString -String "test" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential -argumentList "test", $securePassword
+  $appNameShort = "GitActionsTechInovBCApp"
 
-
-
- Invoke-ScriptInNavContainer -containername $containerName -scriptblock {
-        $appNameShort = "GitActionsTechInovBCApp"
-        Set-NavServerConfiguration 'BC' -KeyName 'EnableUserConsistencyValidationOnTasks' -KeyValue 'false'
-        Write-Host "Restarting Server Instance" -ForegroundColor DarkGreen
-       
-        Write-Host "BC Server Instance" -ForegroundColor DarkGreen  
-
- $ExtensionsToRemove = Get-NavAppInfo -ServerInstance "BC" | Where-Object {($_.Name -eq "$AppName")}
-   Write-Host $ExtensionsToRemove.Name
-    if ($ExtensionsToRemove.count -eq 0)
-   {Write-Host "NothingFound"} 
-   else  
-   
-   {
-   Write-Host "SthFound"
-   UnInstall-NavApp -ServerInstance $serverInstance -Name $appNameShort -Force
-  UnPublish-NavApp -ServerInstance $serverInstance -Name $appNameShort 
-   }
-   }
+  UnInstall-BcContainerApp -containerName $containerName -name $appNameShort -force 
+  UnPublish-BcContainerApp -name $appNameShort -containerName $containerName -force
 
 Compile-AppInNavContainer -appProjectFolder $repositoriesPathHerlp -containerName $containerName -appName $appName -credential $credential
 
 $repositoriesPathHerlp = $repositoriesPathHerlp + "\output\"+ $appName
-Publish-NavContainerApp -containerName $containerName -appFile $repositoriesPathHerlp -skipVerification -sync -install -ignoreIfAppExists 
+Publish-BcContainerApp -containerName $containerName -appFile $repositoriesPathHerlp -skipVerification -sync -install -ignoreIfAppExists
