a0a59d479f010ecebb840f4ef409fb1d71a8f7b5
Mile Slishkovikj <mile.slishkovikj@flaconi.de>
2023-02-24 11:56:25 +0100

    adding PublishAppScript

---
+++ PublishBCApp.ps1
@@ -0,0 +1,11 @@
+﻿$repositoriesPathHerlp = "C:\Users\MileSlishkovikj\source\repos\MyLocalBC\TechInovation"
+$appName = "TechInovation.app"
+$containerName = "MyLocalBC"
+$securePassword = ConvertTo-SecureString -String "test" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -argumentList "test", $securePassword
+
+
+Compile-AppInNavContainer -appProjectFolder $repositoriesPathHerlp -containerName $containerName -appName $appName -credential $credential
+
+$repositoriesPathHerlp = $repositoriesPathHerlp + "\output\"+ $appName
+Publish-NavContainerApp -containerName $containerName -appFile $repositoriesPathHerlp -skipVerification -sync -install
